31bfa00fbf157f13fd92e823efd50d7c7a5c33b9
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-13 11:05:59 +0800

    Time: 0 ms (100.00%) | Memory: 34.1 MB (7.77%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -8,7 +8,7 @@ public:
             int n = sub_vector.size();
             int mid = n/2 + low;
             int mid_value = nums[mid];
-            printf("before %d %d, %d, %d \n", n ,mid, low, high);
+            // printf("before %d %d, %d, %d \n", n ,mid, low, high);
             if (mid_value == target) {
                 return mid;
             } else if (target < mid_value) {
@@ -18,7 +18,7 @@ public:
             }
             if(high<0) return -1;
             if(low>high) return -1;
-            printf("after %d %d, %d, %d \n", n ,mid, low, high);
+            // printf("after %d %d, %d, %d \n", n ,mid, low, high);
             sub_vector = vector<int>(nums.begin()+low, nums.begin()+high);
         }
         return -1;
