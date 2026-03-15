a9b5b9fb3f1050b3df146557d557c47d94611f90
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-15 15:45:05 +0800

    Time: 3 ms (99.60%) | Memory: 55.9 MB (96.01%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -14,22 +14,30 @@ public:
         // function from 21-merge-two-sorted-lists
         if (list1 == nullptr) return list2;
         if (list2 == nullptr) return list1;
-
-        ListNode a(0);
-        ListNode* curr = &a;
+        ListNode* head = nullptr;
+        ListNode* tail = nullptr;
+        if (list1->val < list2->val){
+            head = list1;
+            tail = list1;
+            list1 = list1 -> next;
+        } else {
+            head = list2;
+            tail = list2;
+            list2 = list2 -> next;
+        }
         while (list1 != nullptr && list2 != nullptr){
             if (list1->val < list2->val){
-                curr->next = list1;
+                tail->next = list1;
                 list1 = list1->next;
             } else {
-                curr->next = list2;
+                tail->next = list2;
                 list2 = list2->next;
             }
-            curr = curr->next;
+            tail = tail->next;
         }
-        if (list1 != nullptr) curr->next = list1;
-        if (list2 != nullptr) curr->next = list2;
-        return a.next;
+        if (list1 != nullptr) tail->next = list1;
+        if (list2 != nullptr) tail->next = list2;
+        return head;
     }
     ListNode* getMid(ListNode* head) {
         ListNode* x1 = head;
