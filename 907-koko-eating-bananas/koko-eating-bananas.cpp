3c1350ae45ec2899005144c0d833592740ba97b7
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-16 01:24:27 +0800

    Time: 7 ms (70.14%) | Memory: 22.8 MB (77.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
+    // 傳統二元搜尋的搜尋對象: 數組中現有的索引 (Index)
+    // 搜尋答案 (Binary Search on Answer)的搜尋對象: 滿足條件的數值 (Value)
+    // 在現實問題中，我們常常不知道答案在哪裡，但我們知道答案在哪個範圍內，且具備單調性
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1;
+        int size = piles.size();
         int right = 0;
-        for (int p : piles) right = max(right, p); // 找出最大堆
+        for (int p : piles) right = max(right, p);
 
         int ans = right;
-
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right-left)/2;
             
-            // 驗證「目前猜的這個速度 mid」是否能在 h 內吃完
             if (finishCheck(piles, h, mid)) {
-                ans = mid;      // 記錄可能的答案
-                right = mid - 1; // 速度可行，試試看能不能更慢
+                ans = mid;
+                right = mid-1;
             } else {
-                left = mid + 1;  // 太慢了，必須加快速度
+                left = mid+1;
             }
         }
         return ans;
     }
-
     bool finishCheck(vector<int>& piles, int h, int k) {
-        if (k == 0) return false; // 防止除以 0 的保險
         long t = 0;
         for (int p : piles) {
-            // 使用整數向上取整公式：(p + k - 1) / k
             t += (p + k - 1) / (long)k; 
         }
-        // 如果花費時間 t 小於等於 h，代表速度夠快
         return t <= h;
     }
 };
