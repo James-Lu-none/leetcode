f0b8849d832da5dce385bb1f152949ea42e2aeb5
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-20 11:59:03 +0800

    Time: 8 ms (44.29%) | Memory: 153.7 MB (59.30%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -12,48 +12,48 @@
 
 class Solution {
 public:
-    vector<int> lastVal = {};
+    // vector<int> lastVal = {};
 
-    bool dfs(TreeNode* root, int level) {
-        if (!root) return true;
-        if (root->val % 2 == level % 2) return false;
-        if (lastVal.size() <= level) {
-            lastVal.push_back(root->val);
-        } else {
-            if (level % 2 == 0 && root->val <= lastVal[level]) return false;
-            if (level % 2 == 1 && root->val >= lastVal[level]) return false;
-            lastVal[level] = root->val;
-        }
+    // bool dfs(TreeNode* root, int level) {
+    //     if (!root) return true;
+    //     if (root->val % 2 == level % 2) return false;
+    //     if (lastVal.size() <= level) {
+    //         lastVal.push_back(root->val);
+    //     } else {
+    //         if (level % 2 == 0 && root->val <= lastVal[level]) return false;
+    //         if (level % 2 == 1 && root->val >= lastVal[level]) return false;
+    //         lastVal[level] = root->val;
+    //     }
 
-        return dfs(root->left, level + 1) && dfs(root->right, level + 1);
-    }
-    bool bfs(TreeNode* root) {
-        if (!root) return true;
-        queue<TreeNode*> q;
-        bool isOddLayer = true;
-        q.push(root);
-        while (q.size() != 0) {
-            int size = q.size();
-            int prev = (isOddLayer)?INT_MIN:INT_MAX;
-            for (int i=0;i<size;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                int val = node->val;
-                if (isOddLayer) {
-                    if (val % 2 == 0) return false;
-                    if (val <= prev) return false;
-                } else {
-                    if (val % 2 == 1) return false;
-                    if (val >= prev) return false;
-                }
-                prev = val;
-                if (node->left != nullptr) q.push(node->left);
-                if (node->right != nullptr) q.push(node->right);
-            }
-            isOddLayer = !isOddLayer;
-        }
-        return true;
-    }
+    //     return dfs(root->left, level + 1) && dfs(root->right, level + 1);
+    // }
+    // bool bfs(TreeNode* root) {
+    //     if (!root) return true;
+    //     queue<TreeNode*> q;
+    //     bool isOddLayer = true;
+    //     q.push(root);
+    //     while (q.size() != 0) {
+    //         int size = q.size();
+    //         int prev = (isOddLayer)?INT_MIN:INT_MAX;
+    //         for (int i=0;i<size;i++){
+    //             TreeNode* node = q.front();
+    //             q.pop();
+    //             int val = node->val;
+    //             if (isOddLayer) {
+    //                 if (val % 2 == 0) return false;
+    //                 if (val <= prev) return false;
+    //             } else {
+    //                 if (val % 2 == 1) return false;
+    //                 if (val >= prev) return false;
+    //             }
+    //             prev = val;
+    //             if (node->left != nullptr) q.push(node->left);
+    //             if (node->right != nullptr) q.push(node->right);
+    //         }
+    //         isOddLayer = !isOddLayer;
+    //     }
+    //     return true;
+    // }
 
     bool isEvenOddTree(TreeNode* root) {
         // return dfs(root, 0); //runtime: 8ms, 43.76%, memory: 161MB, 14.66%
