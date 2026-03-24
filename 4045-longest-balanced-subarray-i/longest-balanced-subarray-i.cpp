c41287733e7c12866c85eeb9fbde92d04600e60f
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-24 10:44:23 +0800

    Time: 160 ms (85.72%) | Memory: 282.3 MB (81.31%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
+    // int longestBalanced(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int ans = 0;
+    //     for (int right = 0; right < n; right++) {
+    //         set<int> odd, even;
+    //         // left = r-1~0
+    //         for (int left = right; left >= 0; left--) {
+    //             if (nums[left]%2 == 0) even.insert(nums[left]);
+    //             else odd.insert(nums[left]);
+
+    //             if (odd.size() == even.size()) {
+    //                 ans = max(ans, right-left+1);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
+        vector<int> lastPos(100000 + 1, -1); // 1 <= nums[i] <= 10^5, all element set to -1
+        vector<int> A(n);
+        // r 指向 nums 最後一個元素
+        // 然後從 r 開始，一步一步往前走
+        // 紀錄一個陣列 A[i]，for 每一個 i，如果 nums[i] 是偶數，則 A[i] 就設為 1; 如果 nums[i] 是奇數，則 A[i] 就設為 -1
+        // 如果 lastPos[nums[i]] 是 -1 則把 i 存入 lastPos[nums[i]]，如果 lastPos[nums[i]] 是 n (!= -1)，則把 A[lastPos[nums[i]]] 改為 0
         int ans = 0;
-        for (int right = 0; right < n; right++) {
-            set<int> odd, even;
-            // left = r-1~0
-            for (int left = right; left >= 0; left--) {
-                if (nums[left]%2 == 0) even.insert(nums[left]);
-                else odd.insert(nums[left]);
-
-                if (odd.size() == even.size()) {
-                    ans = max(ans, right-left+1);
+        for (int r = 0; r<n; r++) {
+            A[r] = (nums[r]%2==0)?1:-1;
+            if (lastPos[nums[r]] != -1) {
+                A[lastPos[nums[r]]] = 0;
+            }                
+            lastPos[nums[r]] = r;
+            int sum = 0;
+            for (int l = r;l >= 0;l--) {
+                sum += A[l];
+                if (sum == 0) {
+                    ans = max(ans, r-l+1);
                 }
             }
         }
