22585b85de8c30a3173a5bc5e8702c80052a27b3
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-03-27 22:33:14 +0800

    Time: 7 ms (13.66%) | Memory: 32.4 MB (34.78%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void reverse(vector<int>& v, int l, int r) {
+        while (l<r) {
+            swap(v[l++], v[r--]);
+        }
+    }
+    void leftRotate(vector<int>& v, int n, int k) {
+        if (n<=1) return;
+        k %= n;
+        reverse(v, 0, k-1);
+        reverse(v, k, n-1);
+        reverse(v, 0, n-1);
+    }
+    void rightRotate(vector<int>& v, int n, int k) {
+        if (n<=1) return;
+        k %= n;
+        reverse(v, 0, n-1);
+        reverse(v, 0, k-1);
+        reverse(v, k, n-1);
+    }
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int row = 0, col = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        auto matO(mat);
+        for (int i = 0; i < n; i++) {
+            if (i%2==0){
+                leftRotate(mat[i], m, k);
+                if (matO[i]!=mat[i]) return false;
+            } else {
+                rightRotate(mat[i], m, k);
+                if (matO[i]!=mat[i]) return false;
+            }
+        }
+        return true;
+    }
+};
