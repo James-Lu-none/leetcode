d7992bd7fd7e376c911edd7010498c195711d582
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-04-15 13:32:57 +0800

    Time: 0 ms (100.00%) | Memory: 16.4 MB (27.35%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // find rotate time k, then minimum is at k
-        // find if there is any reverse, if there's any reverse, return num[k], else, return nums[0]
         int n = nums.size();
         int low = 0;
-        int high = n;
-
-        while(low < high) {
-            int mid = low + (high-low)/2;
-            for (int i = low; i<high-1; i++) {
-                if (nums[i]>nums[i+1]) {
-                    return nums[i+1];
-                }
+        int high = n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==nums[high]){
+                high--;
+            }else if(nums[mid]>nums[high]){
+                low=mid+1;
+            }else if(nums[mid]<nums[high]){
+                high=mid;
             }
-            high = mid-1;
-        }        
-        return nums[0];
+        }
+        printf("%d\n", low);
+        return nums[low];
+
+        // find rotate time k, then minimum is at k
+        // find if there is any reverse, if there's any reverse, return num[k], else, return nums[0]
+        // int n = nums.size();
+        // int low = 0;
+        // int high = n;
+
+        // while(low < high) {
+        //     int mid = low + (high-low)/2;
+        //     for (int i = low; i<high-1; i++) {
+        //         if (nums[i]>nums[i+1]) {
+        //             return nums[i+1];
+        //         }
+        //     }
+        //     high = mid-1;
+        // }        
+        // return nums[0];
     }
 };
