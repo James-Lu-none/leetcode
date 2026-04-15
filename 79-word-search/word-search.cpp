4fa2cfa6e0d50cb9441ff96d4c9ce7ea039162d8
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-04-15 11:12:40 +0800

    Time: 659 ms (36.96%) | Memory: 11.8 MB (19.13%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -3,20 +3,18 @@ public:
     int m;
     int n;
     bool dfs(vector<vector<char>>& board, string word, int r, int c, int i){
-        if (r<0 || r>=m || c<0 || c>=n) return false;
         if (board[r][c]!=word[i]) return false;
         if (i == word.length()-1) return true;
         char temp = board[r][c];
         board[r][c] = '.';
-        // bool top = false, down = false, left = false, right = false;
-        // if(r-1>=0) top = dfs(board, word, r-1, c, i+1);
-        // if(r+1<m) down = dfs(board, word, r+1, c, i+1);
-        // if(c-1>=0) left = dfs(board, word, r, c-1, i+1);
-        // if(c+1<n) right = dfs(board, word, r, c+1, i+1);
-        bool result = dfs(board, word, r-1, c, i+1) || dfs(board, word, r+1, c, i+1) || dfs(board, word, r, c-1, i+1) || dfs(board, word, r, c+1, i+1);
+        bool top = false, down = false, left = false, right = false;
+        if(r-1>=0) top = dfs(board, word, r-1, c, i+1);
+        if(r+1<m) down = dfs(board, word, r+1, c, i+1);
+        if(c-1>=0) left = dfs(board, word, r, c-1, i+1);
+        if(c+1<n) right = dfs(board, word, r, c+1, i+1);
         // backtrack: put original value back for next search 
         board[r][c] = temp;
-        return result;
+        return top || down || left || right;
     }
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
