ab9b26aaa0487289c97f1973cd0c7afc274a837d
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-05-27 22:07:38 +0800

    Time: 123 ms (38.00%) | Memory: 114.6 MB (14.40%) - LeetSync

--- 546-remove-boxes/remove-boxes.cpp
+++ 546-remove-boxes/remove-boxes.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+
     int removeBoxes(vector<int>& boxes) {
         int n = boxes.size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(n)));
-            
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k <= j; k++) {
-                dp[j][j][k] = (k + 1) * (k + 1);
-            }
-        }
-            
-        for (int l = 1; l < n; l++) {
-            for (int j = l; j < n; j++) {
-                int i = j - l;
-                    
-                for (int k = 0; k <= i; k++) {
-                    int res = (k + 1) * (k + 1) + dp[i + 1][j][0];
-                        
-                    for (int m = i + 1; m <= j; m++) {
-                        if (boxes[m] == boxes[i]) {
-                            res = max(res, dp[i + 1][m - 1][0] + dp[m][j][k + 1]);
-                        }
-                    }
-                        
-                    dp[i][j][k] = res;
-                }
-            }
-        }
+        return removeBoxesSub(boxes, 0, n - 1, 0, dp);
+    }
+
+    int removeBoxesSub(vector<int>& boxes, int i, int j, int k,  vector<vector<vector<int>>>& dp) {
+        if (i > j) return 0;
+        if (dp[i][j][k] > 0) return dp[i][j][k];
         
-        return (n == 0 ? 0 : dp[0][n - 1][0]);
+        // Need to record the intial values of i and k in order to apply the following optimization
+        int i0 = i;
+        int k0 = k;
+        // optimization: all boxes of the same color counted continuously from the first box should be grouped together
+        for (; i + 1 <= j && boxes[i + 1] == boxes[i]; i++, k++); 
+        int res = (k + 1) * (k + 1) + removeBoxesSub(boxes, i + 1, j, 0, dp);
+        
+        for (int m = i + 1; m <= j; m++) {
+            if (boxes[i] == boxes[m]) {
+                res = max(res, removeBoxesSub(boxes, i + 1, m - 1, 0, dp) + removeBoxesSub(boxes, m, j, k + 1, dp));
+            }
+        }
+            
+        dp[i0][j][k0] = res; // When updating the dp matrix, we should use the initial values of i, j and k
+        return res;
     }
 };
