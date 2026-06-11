e45797a206d658c4a694025f84210d8cbe9618e6
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-06-11 18:23:13 +0800

    Time: 51 ms (89.56%) | Memory: 26.4 MB (87.55%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -8,11 +8,13 @@ public:
         for(int i = 1; i < n; ++i){
             for(int j = 0; j < i; ++j){
                 int diff = stones[i] - stones[j];
-                if(diff < 0 || diff > n || !dp[j][diff]) continue;
-                dp[i][diff] = true;
-                if(diff - 1 >= 0) dp[i][diff - 1] = true;
-                if(diff + 1 <= n) dp[i][diff + 1] = true;
-                if(i == n - 1) return true;
+                if((diff >= 0 && diff <= n) && dp[j][diff]){
+                    if(i == n - 1) return true;
+                    dp[i][diff] = true;
+                    if(diff - 1 >= 0) dp[i][diff - 1] = true;
+                    if(diff + 1 <= n) dp[i][diff + 1] = true;
+                }
+                
             }
         }
 
