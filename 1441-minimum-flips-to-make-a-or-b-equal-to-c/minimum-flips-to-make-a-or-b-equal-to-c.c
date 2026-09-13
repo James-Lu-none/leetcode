4409f328b91bb65153d02fc2549fd6055e40a587
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-09-13 11:36:12 +0800

    Time: 3 ms (49.72%) | Memory: 8.3 MB (85.08%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.c
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.c
@@ -1,17 +1,23 @@
-int count_ones (int x) {
-    int result = 0;
-    while (x>0) {
-        x = x & (x-1);
-        result++;
-    }
-    return result;
-}
+// int count_ones (int x) {
+//     int result = 0;
+//     while (x>0) {
+//         x = x & (x-1);
+//         result++;
+//     }
+//     return result;
+// }
 
 int minFlips(int a, int b, int c){
     int result = 0;
     int f0 = (a | b) ^ c;
     int f1 = (a & b & (~c));
-    result += count_ones(f0);
-    result += count_ones(f1);
+    while (f0>0) {
+        f0 = f0 & (f0-1);
+        result++;
+    }
+    while (f1>0) {
+        f1 = f1 & (f1-1);
+        result++;
+    }
     return result;
 }
