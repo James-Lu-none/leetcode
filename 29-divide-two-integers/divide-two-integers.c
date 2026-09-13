bf4bb43706e0b0c2294c86181e51294742aa2556
Location <59411633+James-Lu-none@users.noreply.github.com>
2026-09-13 13:00:47 +0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (89.97%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.c
+++ 29-divide-two-integers/divide-two-integers.c
@@ -21,7 +21,7 @@ int divide(int dividend, int divisor) {
 
     // return value must reside in [−2^31, 2^31 − 1]
     // so we have to handle edge case where 
-    if (is_negative && quotient > INT_MAX) return INT_MIN;
-     
+    // if (is_negative && quotient > INT_MAX) return INT_MIN;
+
     return is_negative ? -(int)quotient : (int)quotient;
 }
